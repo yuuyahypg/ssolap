e518956969fdb616e004582ea4daa854c5f9ebc0
yuuyahypg <yuuya_hy_pg@yahoo.co.jp>
2017-05-22 08:03:12 +0900

    一旦戻す

--- sb/sink/sink.go
+++ sb/sink/sink.go
@@ -5,19 +5,19 @@ import (
     "gopkg.in/sensorbee/sensorbee.v0/core"
     "gopkg.in/sensorbee/sensorbee.v0/data"
 
-    "github.com/yuuyahypg/ssolap/server"
+    "github.com/yuuyahypg/ssolap/olap"
 
     "fmt"
     //"time"
 )
 
 type Sink struct {
-    server *server.Server
+    olapServer *olap.Server
     count int
 }
 
 func (s *Sink) Write(ctx *core.Context, t *core.Tuple) error {
-    s.server.RecieveTuple(t)
+    s.olapServer.RecieveTuple(t)
     //s.count = s.count + 1
     //if s.count >= 100000 {
     //    fmt.Println(time.Now())
@@ -26,15 +26,15 @@ func (s *Sink) Write(ctx *core.Context, t *core.Tuple) error {
 }
 
 func (s *Sink) Close(ctx *core.Context) error {
-    s.server.Close()
+    s.olapServer.Close()
     return nil
 }
 
 func Create(ctx *core.Context, ioParams *bql.IOParams, params data.Map) (core.Sink, error) {
-    s := server.Run()
+    s := olap.NewServer()
     fmt.Println("success create server")
     return &Sink{
-        server: s,
+        olapServer: s,
         count: 0,
     }, nil
 }
