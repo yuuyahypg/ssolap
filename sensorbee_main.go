89db555a5ec2d9f3eb75e496e9e4e2906fdb498a
yuuyahypg <yuuya_hy_pg@yahoo.co.jp>
2017-06-23 13:05:44 +0900

    .gitignore change

---
+++ sensorbee_main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	_ "github.com/yuuyahypg/ssolap/sender/peopleFlow/plugin"
+	_ "gopkg.in/sensorbee/sensorbee.v0/bql/udf/builtin"
+	"gopkg.in/sensorbee/sensorbee.v0/cmd/lib/run"
+	"gopkg.in/sensorbee/sensorbee.v0/cmd/lib/runfile"
+	"gopkg.in/sensorbee/sensorbee.v0/cmd/lib/shell"
+	"gopkg.in/sensorbee/sensorbee.v0/cmd/lib/topology"
+	"gopkg.in/sensorbee/sensorbee.v0/version"
+	"gopkg.in/urfave/cli.v1"
+	"os"
+	"time"
+)
+
+func init() {
+	// TODO
+	time.Local = time.UTC
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "sensorbee"
+	app.Usage = "SensorBee built with build_sensorbee 0.6.1"
+	app.Version = version.Version
+	app.Commands = []cli.Command{
+		run.SetUp(),
+		runfile.SetUp(),
+		shell.SetUp(),
+		topology.SetUp(),
+	}
+	if err := app.Run(os.Args); err != nil {
+		os.Exit(1)
+	}
+}
